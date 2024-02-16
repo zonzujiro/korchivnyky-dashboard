078c0c731d61ed823acc4221f8115000da1128e7
Ivan Tytarenko <ititarenko@live.com>
2024-02-16 14:21:56 +0200

    fixing jars dynamics

--- src/app/fundraisings/[fundraisingId]/jars/Statistics/analytics.ts
+++ src/app/fundraisings/[fundraisingId]/jars/Statistics/analytics.ts
@@ -20,20 +20,104 @@ const withSign = (value: number) => {
   return value > 0 ? `+${value}` : `${value}`;
 };
 
+const getFirstRecord = (
+  records: Array<JarStatisticRecord>,
+  startDate: Date
+): JarStatisticRecord | null => {
+  const result = records.filter((record) => {
+    const recordDate = new Date(record.createdAt);
+    const isInTimeWindow = isSameDate(recordDate, startDate);
+
+    return isInTimeWindow;
+  });
+
+  if (!result.length) {
+    const recordsBeforeStartDate = records.filter((record) => {
+      const recordDate = new Date(record.createdAt);
+
+      return recordDate.valueOf() <= startDate.valueOf();
+    });
+
+    if (recordsBeforeStartDate.at(-1) !== undefined) {
+      return recordsBeforeStartDate.at(-1) as JarStatisticRecord;
+    }
+
+    return null;
+  }
+
+  return result.at(-1) as JarStatisticRecord;
+};
+
+const getLastRecord = (
+  records: Array<JarStatisticRecord>,
+  startDate: Date,
+  endDate: Date
+) => {
+  const startMs = startDate.valueOf();
+  const endMs = endDate.valueOf();
+
+  const result = records.filter((record) => {
+    const recordDate = new Date(record.createdAt);
+    const isInTimeWindow = isSameDate(recordDate, endDate);
+
+    return isInTimeWindow;
+  });
+
+  if (!result.length) {
+    const recordsBeforeStartDate = records.filter((record) => {
+      const recordDate = new Date(record.createdAt);
+
+      return recordDate.valueOf() <= endMs && recordDate.valueOf() >= startMs;
+    });
+
+    if (recordsBeforeStartDate.length) {
+      return recordsBeforeStartDate.at(-1);
+    }
+
+    // No records since startDate - we will use startDate
+    return null;
+  }
+
+  return result.at(-1);
+};
+
 const filterRecords = (
   jars: Array<Jar>,
   records: Array<JarStatisticRecord>,
   startDate: Date,
   endDate: Date
 ) => {
-  return records.filter((record) => {
+  const selectedJarsRecords = records.filter((record) =>
+    Boolean(jars.find((jar) => jar.id === record.jarId))
+  );
+
+  return selectedJarsRecords.filter((record) => {
     const recordDate = new Date(record.createdAt);
     const isInTimeWindow =
       isSameDate(recordDate, startDate) || isSameDate(recordDate, endDate);
-    const forSelectedJar = Boolean(jars.find((jar) => jar.id === record.jarId));
 
-    return isInTimeWindow && forSelectedJar;
+    return isInTimeWindow;
   });
+};
+
+const getAccumulated = (
+  jarRecords: Array<JarStatisticRecord>,
+  startDate: Date,
+  endDate: Date
+) => {
+  const firstRecord = getFirstRecord(jarRecords, startDate);
+  const lastRecord =
+    getLastRecord(jarRecords, startDate, endDate) || firstRecord;
+
+  const firstAccumulated = firstRecord?.accumulated || 0;
+  const lastAccumulated = lastRecord?.accumulated || 0;
+  const difference = lastAccumulated - firstAccumulated;
+
+  return {
+    firstAccumulated,
+    lastAccumulated,
+    difference,
+  };
 };
 
 export const getAccountsMovements = (
@@ -42,29 +126,36 @@ export const getAccountsMovements = (
   startDate: Date,
   endDate: Date
 ) => {
-  const currentRecords = filterRecords(jars, records, startDate, endDate);
+  const currentRecords = records.filter((record) =>
+    Boolean(jars.find((jar) => jar.id === record.jarId))
+  );
   const groupedByJar = groupBy(currentRecords, (record) => record.jarId);
 
-  console.log({ currentRecords, groupedByJar });
+  const growth = Object.entries(groupedByJar).map(([jarId, jarRecords]) => {
+    const { firstAccumulated, lastAccumulated, difference } = getAccumulated(
+      jarRecords,
+      startDate,
+      endDate
+    );
 
-  const growth = Object.keys(groupedByJar)
-    .filter((jarId) => groupedByJar[jarId].length > 1)
-    .map((jarId) => {
-      const [endDayData, startDayData] = groupedByJar[jarId];
-      const difference = endDayData.accumulated - startDayData.accumulated;
-      const percentageValue =
-        difference > 0
-          ? (100 * difference) / endDayData.accumulated
-          : (100 * difference) / startDayData.accumulated;
+    let percentageValue = 0;
 
-      return {
-        jarId,
-        percentage: `${withSign(Math.round(percentageValue))}%`,
-        difference,
-        startDateAmount: startDayData.accumulated,
-        endDateAmount: endDayData.accumulated,
-      };
-    });
+    if (difference > 0) {
+      percentageValue = (100 * difference) / lastAccumulated;
+    }
+
+    if (difference < 0) {
+      percentageValue = (100 * difference) / firstAccumulated;
+    }
+
+    return {
+      jarId,
+      percentage: `${withSign(Math.round(percentageValue))}%`,
+      difference,
+      startDateAmount: firstAccumulated,
+      endDateAmount: lastAccumulated,
+    };
+  });
 
   return growth;
 };
@@ -78,15 +169,13 @@ export const getGatheringSpeed = (
   const currentRecords = filterRecords(jars, records, startDate, endDate);
   const groupedByJar = groupBy(currentRecords, (record) => record.jarId);
 
-  const speed = Object.keys(groupedByJar)
-    .filter((jarId) => groupedByJar[jarId].length > 1)
-    .map((jarId) => {
-      const [endDayData, startDayData] = groupedByJar[jarId];
-      const difference = endDayData.accumulated - startDayData.accumulated;
-      const hours = getDateDifference(startDate, endDate);
+  const speed = Object.entries(groupedByJar).map(([jarId, jarRecords]) => {
+    const { difference } = getAccumulated(jarRecords, startDate, endDate);
 
-      return { jarId, speed: `${Math.round(difference / hours)} грн/год` };
-    });
+    const hours = getDateDifference(startDate, endDate);
+
+    return { jarId, speed: `${Math.round(difference / hours)} грн/год` };
+  });
 
   return speed;
 };
