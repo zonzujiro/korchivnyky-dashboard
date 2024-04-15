a121d2b2e71fac4cc695b85d425e119e41ce85cf
Serge044 <serhiima@wix.com>
2024-04-15 14:50:12 +0300

    changed config in middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -15,5 +15,5 @@ export const middleware = async (request: NextRequest) => {
 };
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|site-logo.svg).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|.*\\.svg).*)'],
 };
